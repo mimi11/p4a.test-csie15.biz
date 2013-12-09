47da0700029285410d8144dbb9fea59839b9c3b4
Carine <cmelhorn@g.harvard.edu>
2013-12-08 23:14:40 -0500

    renamed database to match new repository

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 define('SYSTEM_EMAIL', 'cmelhorn@g.harvard.edu');
 
 # Default DB name for this app
-define('DB_NAME', "tessie_p4_test-cscie15_biz");
+define('DB_NAME', "tessie_p4a_test-csie15_biz");
 
 # Timezone
 define('TIMEZONE', 'America/New_York');
